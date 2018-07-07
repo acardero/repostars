6eab1861a6d67b7caddd4ed808051f7d28a05003
Alan Cardero <alan.cardero@gmail.com>
2018-07-07 17:56:59 -0400

    Fixed bug with double users

--- routes/auth.js
+++ routes/auth.js
@@ -12,22 +12,16 @@ passport.use(new GitHubStrategy({
     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
   },
   (accessToken, refreshToken, profile, done) => {
-    // const {
-    //     id: githubId, 
-    //     name, 
-    //     login: username, 
-    //     email, 
-    //     avatar_url: avatar
-    // } 
-    // = profile._json;
+    const {
+        id: githubId, 
+        name, 
+        login: username, 
+        email, 
+        avatar_url: avatar
+    } 
+    = profile._json;
 
-    if (profile._json.id)         { var githubId = profile._json.id; }
-    if (profile._json.name)       { var name     = profile._json.name; }
-    if (profile._json.login)      { var username = profile._json.login; }
-    if (profile._json.email)      { var email    = profile._json.email; }
-    if (profile._json.avatar_url) { var avatar   = profile._json.avatar_url; }
-
-    let newUser = new User({
+    const newUser = new User({
       token: accessToken,
       githubId,
       name,
@@ -36,11 +30,15 @@ passport.use(new GitHubStrategy({
       avatar
     });
 
-    console.log('newUser', newUser);
-
-    User.findOrCreate(newUser, function (err, user) {
-        return done(err, user);
-    });
+    User.find({'githubId': githubId}, (err, userCheck)=> {
+        if (err) { res.status(500).send('Something broke!') }
+        if (!userCheck.length) {
+            newUser.save(newUser, (err) => {
+                return done(err, newUser);
+            })
+          }
+        else {return done(err, newUser);}
+    })
 }
 ));
 
